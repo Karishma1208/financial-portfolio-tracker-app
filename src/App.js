37db0dc40d354944a428d0f0b92d11e00dde5b39
Karishma1208 <60914943+Karishma1208@users.noreply.github.com>
2020-07-06 20:22:42 +0530

    fin-portfolio
    Finance Portfolio Tracker

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from './Pages/Main'
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+   </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/Components/ApiCall.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ApiCall extends Component {
+     constructor(props) {
+         super(props)
+     
+         this.state = {
+              livePrice:null,
+              proLoss:null
+         }
+     }
+     
+     componentDidMount(){
+        const{symbol,buyprice,share}=this.props; //Destructuring
+        let sym = symbol;
+        let buy =buyprice;
+        let sh = share;
+        const apikey="HZQKFRQDB2YFHRPZ";
+        const api_url=`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${sym}&apikey=${apikey}`;
+            axios.get(api_url)
+            .then((response)=>{
+                let lastRefreshed=response.data['Meta Data']["3. Last Refreshed"];
+                let date=lastRefreshed.substring(0,10);
+                console.log("final",date)
+                let live = response.data['Time Series (Daily)'][date]["4. close"];
+                let diff = ((live - buy)*sh);
+                diff=diff.toFixed(2);
+                this.setState({
+                    livePrice : live,
+                    proLoss:diff
+                 });
+                 
+            })
+            .catch((error)=>{
+                console.log(error.response);
+                alert('Only 5 API calls can be made per minute,Please reload after sometime for Live Prices');
+                
+            })
+    }
+    render() {
+        return (
+                <>
+                <td>{this.state.livePrice}</td>
+                <td>{this.state.proLoss}</td>
+                </>
+        )
+    }
+}
+
+export default ApiCall;
